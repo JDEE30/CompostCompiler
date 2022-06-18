b57f4a145cecd20840ee43ff27bd99f987776aeb
Jeremy Dickey <47309416+JDEE30@users.noreply.github.com>
2022-06-17 21:59:17 -0500

    initial repo push after development

---
+++ js/compost.js
@@ -0,0 +1,12 @@
+
+
+let VeggieArray = [Lettuce, Cucumber, Carrot, GreenPepper, Tomato, Corn ] //vegie list
+
+VeggieArray[0] = new Image();VeggieArray[0].src = 'images/lettuce.png';
+VeggieArray[1] = new Image();VeggieArray[1].src = 'images/cucumber.png';
+VeggieArray[2] = new Image();VeggieArray[2].src = 'images/cucumber.png';
+VeggieArray[3] = new Image();VeggieArray[3].src = 'images/carrot.png';
+VeggieArray[4] = new Image();VeggieArray[4].src = 'images/greenPepper.png';
+VeggieArray[5] = new Image();VeggieArray[5].src = 'images/Tomato.png';
+VeggieArray[6] = new Image();VeggieArray[6].src = 'images/Tomato.png';
+/* images for objects in array*/
